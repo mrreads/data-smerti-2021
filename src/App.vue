0381658dba043a1128780a96ea46f1450f5f7f65
Alexander <mrreads@yandex.com>
2021-09-23 14:28:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Langing> </Langing>
+</template>
+
+<script>
+import './assets/css/general.css';
+
+import Langing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Langing
+  }
+}
+</script>
