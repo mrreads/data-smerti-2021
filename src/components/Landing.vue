cb98b41b9b319a4f6d3e4c11d4c3c2baaf61be7f
Alexander <mrreads@yandex.com>
2021-09-23 15:23:37 +0300

    section one

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>landing</h1>
+  <div class="section_one">
+    <div class="container">
+      <p class="text-mini">Лучшие астрологи и экстрасенсы Румынии</p>
+      <hr>
+      <p class="text-large">Точность прогноза: 97%</p>
+
+      <div class="border-wrapper">
+        <img class="image" src="./../assets/img/section1_image.png" alt="Гадалка">
+        <img class="border" src="./../assets/img/section1_border.png" alt="Гадалка">
+      </div>
+      <p class="text-orange">Вас беспокоит вопрос о том, <br> КОГДА ВЫ ПОКИНИТЕ ЭТОТ МИР И ПРИ <br> КАКИХ ОБСТОЯТЕЛЬСТВАХ? </p>
+      
+      <button class="button left-blick"> Да</button>
+      <button class="button right-blick"> Нет </button>
+
+      <p class="text-mini">Онлайн предсказание</p>
+    </div>
+  </div>
 </template>
