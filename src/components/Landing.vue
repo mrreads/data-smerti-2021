e3f287f6c7f323acce95a00b9c83cdcc3b4382d0
Alexander <mrreads@yandex.com>
2021-09-23 15:38:24 +0300

    section two

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div class="section_one">
     <div class="container">
       <p class="text-mini">Лучшие астрологи и экстрасенсы Румынии</p>
@@ -6,8 +7,8 @@
       <p class="text-large">Точность прогноза: 97%</p>
 
       <div class="border-wrapper">
-        <img class="image" src="./../assets/img/section1_image.png" alt="Гадалка">
-        <img class="border" src="./../assets/img/section1_border.png" alt="Гадалка">
+        <img class="image" src="./../assets/img/section1_image.png" alt="Девушка">
+        <img class="border" src="./../assets/img/section1_border.png" alt="Рамка">
       </div>
       <p class="text-orange">Вас беспокоит вопрос о том, <br> КОГДА ВЫ ПОКИНИТЕ ЭТОТ МИР И ПРИ <br> КАКИХ ОБСТОЯТЕЛЬСТВАХ? </p>
       
@@ -17,4 +18,18 @@
       <p class="text-mini">Онлайн предсказание</p>
     </div>
   </div>
+
+  <div class="section_two">
+    <div class="container">
+      <div class="border">
+        <img class="image" src="./../assets/img/section2_image.png" alt="Руки">
+          <p>Позвольте нам раскрыть эту <br> волнующую тайну и <br>
+          <span>с точностью определить дату <br>
+          и время вашей смерти, </span>а <br>
+          также предшествующую <br>
+          этому событию причину </p>
+      </div>
+    </div>
+  </div>
+
 </template>
